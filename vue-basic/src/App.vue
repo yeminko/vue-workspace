8c37e0a8e1c009c210013daef240eff83c5e23f6
yeminko <yeminko47@gmail.com>
2024-12-23 21:00:35 +0630

    Code sample for Text Interpolation article

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <section></section>
+  <h1>{{ greet }}</h1>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const greet = ref('Hi John!')
+</script>
 
 <style scoped lang="scss"></style>

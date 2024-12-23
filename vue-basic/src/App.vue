be67e3148efa7b6981dc2eeaaf7498b53476edcd
yeminko <yeminko47@gmail.com>
2024-12-24 00:08:18 +0630

    feat: update App.vue to display count and add increment button

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <h1>{{ greet }}</h1>
+  <h1>Count : {{ count }}</h1>
+  <button @focus="increaseCount">Increment</button>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const greet = ref('Hi John!')
+const count = ref(0)
+
+function increaseCount() {
+  count.value++
+}
 </script>
 
 <style scoped lang="scss"></style>

8dfe45737bf17b7c3a2c4f16205d75a8482728bc
yeminko <yeminko47@gmail.com>
2025-01-04 14:03:00 +0630

    feat: update App component to include SimpleComponent and pass username prop

--- vue-basic-02/src/App.vue
+++ vue-basic-02/src/App.vue
@@ -1,5 +1,12 @@
-<template><section></section></template>
+<template>
+  <SimpleComponent :username="name" />
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import SimpleComponent from './components/SimpleComponent.vue'
+
+const name = ref('John Doe')
+</script>
 
 <style scoped lang="scss"></style>
---
+++ vue-basic-02/src/components/SimpleComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Welcome {{ username }},</h1>
+</template>
+
+<script setup lang="ts">
+defineProps<{ username: string }>()
+</script>
+
+<style scoped lang="scss"></style>
